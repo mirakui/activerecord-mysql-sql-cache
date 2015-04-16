53cafbb9a491f2dcfbd065fc23a77d89ba5f7a2e
Issei Naruta <naruta@cookpad.com>
2015-04-16 11:44:10 +0900

    First commit

---
+++ lib/activerecord/mysql/sql/cache.rb
@@ -0,0 +1,11 @@
+require "activerecord/mysql/sql/cache/version"
+
+module Activerecord
+  module Mysql
+    module Sql
+      module Cache
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/activerecord/mysql/sql/cache/version.rb
@@ -0,0 +1,9 @@
+module Activerecord
+  module Mysql
+    module Sql
+      module Cache
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
