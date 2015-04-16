a4f6ced62519676dcee9067efd31e88fc09d5dd5
Issei Naruta <naruta@cookpad.com>
2015-04-16 19:57:13 +0900

    Fix bugs around ActiveRecord::Relation.clone

--- lib/activerecord-mysql-sql-cache/patches/active_record/relation.rb
+++ lib/activerecord-mysql-sql-cache/patches/active_record/relation.rb
@@ -1,18 +1,39 @@
+require 'active_support/concern'
+
 module ActiverecordMysqlSqlCache
   module Patches
     module ActiveRecord
       module Relation
+        extend ActiveSupport::Concern
+
+        def mysql_sql_cache_value=(value)
+          @values[:mysql_sql_cache] = value
+        end
+
+        def mysql_sql_cache_value
+          @values[:mysql_sql_cache]
+        end
+
         def sql_cache(enabled=true)
           if enabled.nil?
-            arel.mysql_sql_cache = nil
+            self.mysql_sql_cache_value = nil
           else
-            arel.mysql_sql_cache = enabled ? ' SQL_CACHE ' : ' SQL_NO_CACHE '
+            self.mysql_sql_cache_value = enabled ? ' SQL_CACHE ' : ' SQL_NO_CACHE '
           end
           self
         end
 
         def sql_no_cache
           sql_cache(false)
+        end
+
+        included do
+          def build_arel_with_sql_cache
+            build_arel_without_sql_cache.tap do |arel|
+              arel.mysql_sql_cache = self.mysql_sql_cache_value
+            end
+          end
+          alias_method_chain :build_arel, :sql_cache
         end
       end
     end
